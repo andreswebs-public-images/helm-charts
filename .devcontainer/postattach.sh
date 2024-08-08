ce541360ed34245e998b65377dc2b8d0c838b14b
Andre Silva <andreswebs@pm.me>
2024-08-08 16:55:16 -0300

    PR workflow

--- .devcontainer/postattach.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Commands to run after attaching to the Container
-
-## pre-commit
-pre-commit install
--- .devcontainer/poststart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Commands to run after the Container is started
-
-# git
-git config --global --add safe.directory "$1"
